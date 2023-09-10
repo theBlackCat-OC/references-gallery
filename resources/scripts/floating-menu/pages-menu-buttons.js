5056ebe852055b55ff9d6b03a33e5e84bccbacaf
the Black Cat <contact@theblack.cat>
2023-09-10 02:10:03 +0200

    Revamped floating navigation menu

---
+++ resources/scripts/floating-menu/pages-menu-buttons.js
@@ -0,0 +1,11 @@
+document.getElementById('back-menu-button').onclick = function() {
+  window.location.href = 'https://github.com/theBlackCat-OC/references-gallery';
+};
+
+document.getElementById('sitemap-menu-button').onclick = function() {
+    window.location.href = 'https://theblack.cat/?link=tbc';
+};
+
+document.getElementById('home-menu-button').onclick = function() {
+    window.location.href = 'https://skeb.theblack.cat';
+};
