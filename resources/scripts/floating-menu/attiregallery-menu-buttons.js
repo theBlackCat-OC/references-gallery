084e7a3fa41465b9ef843437d1bfa00d7a456be1
the Black Cat <contact@theblack.cat>
2023-09-10 02:13:39 +0200

    Updated floating menu buttons

--- resources/scripts/floating-menu/attiregallery-menu-buttons.js
+++ resources/scripts/floating-menu/attiregallery-menu-buttons.js
@@ -1,11 +1,11 @@
 document.getElementById('back-menu-button').onclick = function() {
-  window.location.href = 'https://theblack.cat?link=author';
+  window.location.href = 'https://github.com/theBlackCat-OC/references-gallery';
 };
 
 document.getElementById('sitemap-menu-button').onclick = function() {
-    window.location.href = 'site-map';
+    window.location.href = 'https://theblack.cat/?link=tbc';
 };
 
 document.getElementById('home-menu-button').onclick = function() {
-    window.location.href = 'https://theblack.cat';
+    window.location.href = 'https://skeb.theblack.cat/attire-gallery';
 };
