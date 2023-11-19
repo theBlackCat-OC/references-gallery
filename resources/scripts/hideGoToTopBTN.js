0317787efbbd0b5aef2bc28d37fb78c9350367b7
the Black Cat <contact@theblack.cat>
2023-11-19 14:13:16 +0100

    Updates

--- resources/scripts/hideGoToTopBTN.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function() {
-    var goTopBtn = document.getElementById("goTopBtn");
-    if (window.pageYOffset > 100) {
-        goTopBtn.style.display = "block";
-    } else {
-        goTopBtn.style.display = "none";
-    }
-});
